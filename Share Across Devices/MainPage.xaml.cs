315aec23e7af2a2c15f5f91859a3bdacacf59efb
Steven Countryman <stevenmcountryman@outlook.com>
2016-08-08 07:26:33 -0400

    Add project files.

---
+++ Share Across Devices/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Navigation;
+
+namespace Share_Across_Devices
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static MainPage Current;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Current = this;
+        }
+        private void ScenarioControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Clear the status block when navigating scenarios.
+            NotifyUser(String.Empty);
+
+            ListBox scenarioListBox = sender as ListBox;
+            Scenario s = scenarioListBox.SelectedItem as Scenario;
+            if (s != null)
+            {
+                ScenarioFrame.Navigate(s.ClassType);
+            }
+        }
+
+        /// <summary>
+        /// Used to display messages to the user
+        /// </summary>
+        /// <param name="strMessage"></param>
+        /// <param name="type"></param>
+        public void NotifyUser(string strMessage)
+        {
+            StatusBlock.Text = strMessage;
+        }
+
+        public List<Scenario> Scenarios
+        {
+            get { return this.scenarios; }
+        }
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Populate the scenario list from the SampleConfiguration.cs file
+            ScenarioControl.ItemsSource = scenarios;
+            if (Window.Current.Bounds.Width < 640)
+            {
+                ScenarioControl.SelectedIndex = -1;
+            }
+            else
+            {
+                ScenarioControl.SelectedIndex = 0;
+            }
+        }
+
+        public enum NotifyType
+        {
+            StatusMessage,
+            ErrorMessage
+        };
+    }
+
+    public class ScenarioBindingConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Scenario s = value as Scenario;
+            return (MainPage.Current.Scenarios.IndexOf(s) + 1) + ") " + s.Title;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return true;
+        }
+    }
+}
---
+++ Share Across Devices/ShareWebLink.xaml.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.ApplicationModel.DataTransfer.ShareTarget;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.System;
+using Windows.System.RemoteSystems;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Navigation;
+using static Share_Across_Devices.MainPage;
+
+namespace Share_Across_Devices
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ShareWebLink : Page
+    {
+        ShareOperation shareOperation;
+        private string sharedDataTitle;
+        private string sharedDataDescription;
+        private string sharedDataPackageFamilyName;
+        private Uri sharedDataContentSourceWebLink;
+        private Uri sharedDataContentSourceApplicationLink;
+        private Color sharedDataLogoBackgroundColor;
+        private IRandomAccessStreamReference sharedDataSquare30x30Logo;
+        private string shareQuickLinkId;
+        private string sharedText;
+        private Uri sharedWebLink;
+        private Uri sharedApplicationLink;
+        private IReadOnlyList<IStorageItem> sharedStorageItems;
+        private string sharedCustomData;
+        private string sharedHtmlFormat;
+        private IReadOnlyDictionary<string, RandomAccessStreamReference> sharedResourceMap;
+        private IRandomAccessStreamReference sharedBitmapStreamRef;
+        private IRandomAccessStreamReference sharedThumbnailStreamRef;
+        private const string dataFormatName = "http://schema.org/Book";
+        private ObservableCollection<RemoteSystem> deviceList;
+        private RemoteSystemWatcher deviceWatcher;
+
+        public ObservableCollection<RemoteSystem> DeviceList
+        {
+            get
+            {
+                return this.deviceList;
+            }
+        }
+
+        public ShareWebLink()
+        {
+            this.InitializeComponent();
+
+            this.setUpDevicesList();
+        }
+
+        private async void setUpDevicesList()
+        {
+            deviceList = new ObservableCollection<RemoteSystem>();
+            RemoteSystemAccessStatus accessStatus = await RemoteSystem.RequestAccessAsync();
+            await RemoteSystem.RequestAccessAsync();
+            deviceWatcher = RemoteSystem.CreateWatcher();
+            deviceWatcher.RemoteSystemAdded += DeviceWatcher_RemoteSystemAdded;
+            deviceWatcher.Start();
+        }
+
+        private async void DeviceWatcher_RemoteSystemAdded(RemoteSystemWatcher sender, RemoteSystemAddedEventArgs args)
+        {
+            var remoteSystem = args.RemoteSystem;
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                if (!this.deviceList.Contains(remoteSystem))
+                {
+                    this.deviceList.Add(remoteSystem);
+                }
+                this.DeviceListBox.ItemsSource = this.deviceList;
+            });
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // It is recommended to only retrieve the ShareOperation object in the activation handler, return as
+            // quickly as possible, and retrieve all data from the share target asynchronously.
+
+            this.shareOperation = (ShareOperation)e.Parameter;
+
+            await Task.Factory.StartNew(async () =>
+            {
+                // Retrieve the data package properties.
+                this.sharedDataTitle = this.shareOperation.Data.Properties.Title;
+                this.sharedDataDescription = this.shareOperation.Data.Properties.Description;
+                this.sharedDataPackageFamilyName = this.shareOperation.Data.Properties.PackageFamilyName;
+                this.sharedDataContentSourceWebLink = this.shareOperation.Data.Properties.ContentSourceWebLink;
+                this.sharedDataContentSourceApplicationLink = this.shareOperation.Data.Properties.ContentSourceApplicationLink;
+                this.sharedDataLogoBackgroundColor = this.shareOperation.Data.Properties.LogoBackgroundColor;
+                this.sharedDataSquare30x30Logo = this.shareOperation.Data.Properties.Square30x30Logo;
+                this.sharedThumbnailStreamRef = this.shareOperation.Data.Properties.Thumbnail;
+                this.shareQuickLinkId = this.shareOperation.QuickLinkId;
+
+                // Retrieve the data package content.
+                // The GetWebLinkAsync(), GetTextAsync(), GetStorageItemsAsync(), etc. APIs will throw if there was an error retrieving the data from the source app.
+                // In this sample, we just display the error. It is recommended that a share target app handles these in a way appropriate for that particular app.
+                if (this.shareOperation.Data.Contains(StandardDataFormats.WebLink))
+                {
+                    try
+                    {
+                        this.sharedWebLink = await this.shareOperation.Data.GetWebLinkAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        NotifyUserBackgroundThread("Failed GetWebLinkAsync - " + ex.Message, NotifyType.ErrorMessage);
+                    }
+                }
+                if (this.shareOperation.Data.Contains(StandardDataFormats.ApplicationLink))
+                {
+                    try
+                    {
+                        this.sharedApplicationLink = await this.shareOperation.Data.GetApplicationLinkAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        NotifyUserBackgroundThread("Failed GetApplicationLinkAsync - " + ex.Message, NotifyType.ErrorMessage);
+                    }
+                }
+                if (this.shareOperation.Data.Contains(StandardDataFormats.Text))
+                {
+                    try
+                    {
+                        this.sharedText = await this.shareOperation.Data.GetTextAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        NotifyUserBackgroundThread("Failed GetTextAsync - " + ex.Message, NotifyType.ErrorMessage);
+                    }
+                }
+                if (this.shareOperation.Data.Contains(StandardDataFormats.StorageItems))
+                {
+                    try
+                    {
+                        this.sharedStorageItems = await this.shareOperation.Data.GetStorageItemsAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        NotifyUserBackgroundThread("Failed GetStorageItemsAsync - " + ex.Message, NotifyType.ErrorMessage);
+                    }
+                }
+                if (this.shareOperation.Data.Contains(dataFormatName))
+                {
+                    try
+                    {
+                        this.sharedCustomData = await this.shareOperation.Data.GetTextAsync(dataFormatName);
+                    }
+                    catch (Exception ex)
+                    {
+                        NotifyUserBackgroundThread("Failed GetTextAsync(" + dataFormatName + ") - " + ex.Message, NotifyType.ErrorMessage);
+                    }
+                }
+                if (this.shareOperation.Data.Contains(StandardDataFormats.Html))
+                {
+                    try
+                    {
+                        this.sharedHtmlFormat = await this.shareOperation.Data.GetHtmlFormatAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        NotifyUserBackgroundThread("Failed GetHtmlFormatAsync - " + ex.Message, NotifyType.ErrorMessage);
+                    }
+
+                    try
+                    {
+                        this.sharedResourceMap = await this.shareOperation.Data.GetResourceMapAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        NotifyUserBackgroundThread("Failed GetResourceMapAsync - " + ex.Message, NotifyType.ErrorMessage);
+                    }
+                }
+                if (this.shareOperation.Data.Contains(StandardDataFormats.Bitmap))
+                {
+                    try
+                    {
+                        this.sharedBitmapStreamRef = await this.shareOperation.Data.GetBitmapAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        NotifyUserBackgroundThread("Failed GetBitmapAsync - " + ex.Message, NotifyType.ErrorMessage);
+                    }
+                }
+
+                // In this sample, we just display the shared data content.
+
+                // Get back to the UI thread using the dispatcher.
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    if (this.sharedWebLink != null)
+                    {
+                        AddContentValue("WebLink: ", this.sharedWebLink.AbsoluteUri);
+                    }
+                });
+            });
+        }
+
+        private async void DismissUI_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedDevice = this.deviceList[this.DeviceListBox.SelectedIndex];
+
+            if (selectedDevice != null)
+            {
+                Uri uri;
+                if (Uri.TryCreate(this.sharedWebLink.AbsoluteUri, UriKind.Absolute, out uri))
+                {
+                    this.LoadingBar.IsEnabled = true;
+                    this.LoadingBar.Visibility = Visibility.Visible;
+                    RemoteLaunchUriStatus launchUriStatus = await RemoteLauncher.LaunchUriAsync(new RemoteSystemConnectionRequest(selectedDevice), uri);
+                    this.LoadingBar.IsEnabled = false;
+                    this.LoadingBar.Visibility = Visibility.Collapsed;
+                    this.shareOperation.DismissUI();
+                }
+            }
+        }
+
+        private void ReportDataRetrieved_Click(object sender, RoutedEventArgs e)
+        {
+            this.shareOperation.ReportDataRetrieved();
+            this.NotifyUser("Data retrieved...");
+        }
+
+        async private void Footer_Click(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri(((HyperlinkButton)sender).Tag.ToString()));
+        }
+
+        private void NotifyUser(string strMessage)
+        {
+            StatusBlock.Text = strMessage;
+        }
+
+        async private void NotifyUserBackgroundThread(string message, NotifyType type)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                NotifyUser(message);
+            });
+        }
+
+        private void AddContentValue(string title, string description = null)
+        {
+            Run contentType = new Run();
+            contentType.FontWeight = FontWeights.Bold;
+            contentType.Text = title;
+            ContentValue.Inlines.Add(contentType);
+
+            if (description != null)
+            {
+                Run contentValue = new Run();
+                contentValue.Text = description + Environment.NewLine;
+                ContentValue.Inlines.Add(contentValue);
+            }
+        }
+
+        private void DeviceListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.DeviceListBox.SelectedIndex >= 0)
+            {
+                this.DismissUI.IsEnabled = true;
+            }
+        }
+    }
+}
