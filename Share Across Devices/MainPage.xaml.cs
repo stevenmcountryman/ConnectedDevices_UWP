406be1774262769cd2047b2670447af3033b217f
Steven Countryman <stevenmcountryman@outlook.com>
2016-08-08 15:13:49 -0400

    Added App Service to connect remotely

--- Share Across Devices/MainPage.xaml.cs
+++ Share Across Devices/MainPage.xaml.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation.Collections;
+using Windows.System.RemoteSystems;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
@@ -13,14 +19,165 @@ namespace Share_Across_Devices
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public static MainPage Current;
+        private ObservableCollection<RemoteSystem> deviceList;
+        private RemoteSystemWatcher deviceWatcher;
+
+        public ObservableCollection<RemoteSystem> DeviceList
+        {
+            get
+            {
+                return this.deviceList;
+            }
+        }
 
         public MainPage()
         {
             this.InitializeComponent();
-            Current = this;
+
+            this.setupClipboardText();
+
+            this.setUpDevicesList();
+        }
+
+        private async void setupClipboardText()
+        {
+            DataPackageView dataPackageView = Clipboard.GetContent();
+            if (dataPackageView.Contains(StandardDataFormats.Text))
+            {
+                string text = await dataPackageView.GetTextAsync();
+
+                this.ClipboardText.Text = text;
+            }
+        }
+
+        private async void setUpDevicesList()
+        {
+            deviceList = new ObservableCollection<RemoteSystem>();
+            RemoteSystemAccessStatus accessStatus = await RemoteSystem.RequestAccessAsync();
+            await RemoteSystem.RequestAccessAsync();
+            deviceWatcher = RemoteSystem.CreateWatcher();
+            deviceWatcher.RemoteSystemAdded += DeviceWatcher_RemoteSystemAdded;
+            deviceWatcher.Start();
+        }
+
+        private async void DeviceWatcher_RemoteSystemAdded(RemoteSystemWatcher sender, RemoteSystemAddedEventArgs args)
+        {
+            var remoteSystem = args.RemoteSystem;
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                if (!this.deviceList.Contains(remoteSystem))
+                {
+                    this.deviceList.Add(remoteSystem);
+                }
+                this.DeviceListBox.ItemsSource = this.deviceList;
+            });
+        }
+
+        private async void ShareButton_Click(object sender, RoutedEventArgs e)
+        {
+            RemoteSystem selectedDevice = this.DeviceListBox.SelectedItem as RemoteSystem;
+            if (selectedDevice != null)
+            {
+                // Create a remote system connection request.
+                RemoteSystemConnectionRequest connectionRequest = new RemoteSystemConnectionRequest(selectedDevice);
+
+                // Set up a new app service connection. The following app service name and package family name
+                // are used in this sample to work with AppServices provider SDK sample on a remote system.
+                using (AppServiceConnection connection = new AppServiceConnection
+                {
+                    AppServiceName = "com.simplisidy.appservice",
+                    PackageFamilyName = "Simplisidy.UWP.ShareAcrossDevices.CS_wtkr3v20s86d8"
+                }) 
+                {
+                    NotifyUser("Opening connection to " + selectedDevice.DisplayName + "...", NotifyType.StatusMessage);
+                    AppServiceConnectionStatus status = await connection.OpenRemoteAsync(connectionRequest);
+
+                    if (status == AppServiceConnectionStatus.Success)
+                    {
+                        NotifyUser("Successfully connected to " + selectedDevice.DisplayName + "...", NotifyType.StatusMessage);
+                        await SendMessageToRemoteAppServiceAsync(connection);
+                    }
+                    else
+                    {
+                        NotifyUser("Attempt to open a remote app service connection failed with error - " + status.ToString(), NotifyType.ErrorMessage);
+                    }
+                }
+            }
+            else
+            {
+                NotifyUser("Select a device for remote connection.", NotifyType.ErrorMessage);
+            }
+        }
+        private async Task SendMessageToRemoteAppServiceAsync(AppServiceConnection connection)
+        {
+            // Send message if connection to the remote app service is open.
+            if (connection != null)
+            {
+                //Set up the inputs and send a message to the service.
+                ValueSet inputs = new ValueSet();
+                inputs.Add("clipboard", this.ClipboardText.Text);
+                NotifyUser("Sent clipboard to " + (this.DeviceListBox.SelectedItem as RemoteSystem).DisplayName + ". Waiting for a response...", NotifyType.StatusMessage);
+                AppServiceResponse response = await connection.SendMessageAsync(inputs);
+
+                if (response.Status == AppServiceResponseStatus.Success)
+                {
+                    if (response.Message.ContainsKey("result"))
+                    {
+                        string resultText = response.Message["result"].ToString();
+                        if (string.IsNullOrEmpty(resultText))
+                        {
+                            NotifyUser("Remote app service did not respond with a result.", NotifyType.ErrorMessage);
+                        }
+                        else
+                        {
+                            NotifyUser("Result = " + resultText, NotifyType.StatusMessage);
+                        }
+                    }
+                    else
+                    {
+                        NotifyUser("Response from remote app service does not contain a result.", NotifyType.ErrorMessage);
+                    }
+                }
+                else
+                {
+                    NotifyUser("Sending message to remote app service failed with error - " + response.Status.ToString(), NotifyType.ErrorMessage);
+                }
+            }
+            else
+            {
+                NotifyUser("Not connected to any app service. Select a device to open a connection.", NotifyType.ErrorMessage);
+            }
+        }
+        public void NotifyUser(string strMessage, NotifyType type)
+        {
+            StatusBlock.Text = strMessage;
+        }
+
+        private void ClipboardText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.ClipboardText.Text.Length > 0)
+            {
+                this.ShareButton.IsEnabled = true;
+            }
+            else
+            {
+                this.ShareButton.IsEnabled = false;
+            }
+        }
+
+        private void DeviceListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.DeviceListBox.SelectedIndex >= 0)
+            {
+                this.ShareButton.IsEnabled = true;
+            }
         }
     }
+    public enum NotifyType
+    {
+        StatusMessage,
+        ErrorMessage
+    };
 
     sealed partial class App : Application
     {
---
+++ ShareAcrossAppService/AppService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation.Collections;
+
+namespace ShareAcrossAppService
+{
+    public sealed class AppService : IBackgroundTask
+    {
+        private BackgroundTaskDeferral backgroundTaskDeferral;
+        private AppServiceConnection appServiceconnection;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            this.backgroundTaskDeferral = taskInstance.GetDeferral(); // Get a deferral so that the service isn't terminated.
+            taskInstance.Canceled += OnTaskCanceled; // Associate a cancellation handler with the background task.
+
+            // Retrieve the app service connection and set up a listener for incoming app service requests.
+            var details = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+            appServiceconnection = details.AppServiceConnection;
+            appServiceconnection.RequestReceived += OnRequestReceived;
+        }
+
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            if (this.backgroundTaskDeferral != null)
+            {
+                this.backgroundTaskDeferral.Complete();
+                this.backgroundTaskDeferral = null;
+            }
+        }
+
+        async void OnRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            //Get a deferral so we can use an awaitable API to respond to the message
+            var messageDeferral = args.GetDeferral();
+
+            try
+            {
+                var input = args.Request.Message;
+                string clipboard = (string)input["clipboard"];
+                DataPackage dataPackage = new DataPackage();
+                dataPackage.RequestedOperation = DataPackageOperation.Copy;
+                dataPackage.SetText(clipboard);
+                Clipboard.SetContent(dataPackage);
+                //Create the response
+                var result = new ValueSet();
+                result.Add("result", "Copied to clipboard!");
+
+                //Send the response
+                await args.Request.SendResponseAsync(result);
+
+            }
+            finally
+            {
+                //Complete the message deferral so the platform knows we're done responding
+                messageDeferral.Complete();
+            }
+        }
+    }
+}
